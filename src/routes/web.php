aec9dbc94fb03fa1a52293d484a431f00792111f
Jahnavi <jahnavi.mathur@dotsquares.com>
2025-07-23 11:04:07 +0530

    ui issue fixed

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use admin\emails\Controllers\EmailManagerController;
 
-Route::name('admin.')->middleware(['web','auth:admin'])->group(function () {  
-    Route::middleware('auth:admin')->group(function () {
-        Route::resource('emails', EmailManagerController::class);
-        Route::post('emails/updateStatus', [EmailManagerController::class, 'updateStatus'])->name('emails.updateStatus');
-    });
+Route::name('admin.')->middleware(['web','admin.auth'])->group(function () {  
+    Route::resource('emails', EmailManagerController::class);
+    Route::post('emails/updateStatus', [EmailManagerController::class, 'updateStatus'])->name('emails.updateStatus');
 });
